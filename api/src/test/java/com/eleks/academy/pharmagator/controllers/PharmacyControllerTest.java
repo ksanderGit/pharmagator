9e48a180bca29912f5fdbd220ebcc2a9e0ac8d0e
ksander <ksander.a.a@gmail.com>
2021-11-20 15:04:12 +0200

    added PharmacyControllerTest.class

---
+++ api/src/test/java/com/eleks/academy/pharmagator/controllers/PharmacyControllerTest.java
@@ -0,0 +1,28 @@
+package com.eleks.academy.pharmagator.controllers;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PharmacyControllerTest {
+
+    @Test
+    void getAll() {
+    }
+
+    @Test
+    void getPharmacyById() {
+    }
+
+    @Test
+    void addPharmacy() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void deletePharmacy() {
+    }
+}
